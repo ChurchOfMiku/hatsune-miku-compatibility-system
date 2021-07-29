1f5966af95190dbb5d2d95838c8982e547f3609f
GGG KILLER <gggkiller2@gmail.com>
2021-07-29 14:37:53 -0300

    Update the run commands.
    Lets the user run a file or a single-line snippet.

--- .console/Program.cs
+++ .console/Program.cs
@@ -1,4 +1,4 @@
-#define PROFILING
+//#define PROFILING
 //#define TEST_HASH
 
 using System.Diagnostics;
@@ -12,23 +12,25 @@ using Tsu.Numerics;
 
 namespace Miku.Console
 {
-    using System;
+	using System;
+	using System.Threading;
 
-    public static class Program
-    {
-        private static readonly Action profilerDump =
-            typeof( Profiler ).GetMethod( "Dump", BindingFlags.Static | BindingFlags.NonPublic )
-                              .CreateDelegate<Action>();
-        private static ConsoleCommandManager _commandManager;
-        private static LuaMachine _luaMachine;
-        private static Function _compile;
+	public static class Program
+	{
+		private static readonly Action profilerDump =
+			typeof( Profiler ).GetMethod( "Dump", BindingFlags.Static | BindingFlags.NonPublic )
+							  .CreateDelegate<Action>();
+		private static ConsoleCommandManager _commandManager;
+		private static LuaMachine _luaMachine;
+		private static Function _compile;
+		private static int _consoleCodeCounter = 0;
 
-        public static void Main()
-        {
-            if ( !Directory.Exists( "data" ) )
-            {
-                Directory.CreateDirectory( "data" );
-            }
+		public static void Main()
+		{
+			if ( !Directory.Exists( "data" ) )
+			{
+				Directory.CreateDirectory( "data" );
+			}
 #if TEST_HASH
 			Lua.Objects.Internal.MikuDict.Bench();
 			return;
@@ -36,14 +38,14 @@ namespace Miku.Console
 
 #if !PROFILING
 			_commandManager = new();
-            _commandManager.LoadCommands( typeof( Program ), null );
-            _commandManager.AddHelpCommand();
-            _commandManager.AddStopCommand( "exit", "stop", "quit" );
+			_commandManager.LoadCommands( typeof( Program ), null );
+			_commandManager.AddHelpCommand();
+			_commandManager.AddStopCommand( "exit", "stop", "quit" );
 #endif
-            _luaMachine = new LuaMachine();
-            _compile = (Function)typeof( LuaMachine )
-                .GetField( "CompileFunction", BindingFlags.NonPublic | BindingFlags.Instance )
-                .GetValue( _luaMachine );
+			_luaMachine = new LuaMachine();
+			_compile = (Function)typeof( LuaMachine )
+				.GetField( "CompileFunction", BindingFlags.NonPublic | BindingFlags.Instance )
+				.GetValue( _luaMachine );
 
 #if PROFILING
 			var fullRunTimer = Stopwatch.StartNew();
@@ -54,46 +56,57 @@ namespace Miku.Console
 			LuaStats();
 			Sandbox.Log.Info( "Full runtime: " + fullRunTimer.Elapsed.TotalSeconds );
 #else
-            _commandManager.Start();
+			_commandManager.Start();
 #endif
 		}
 
-        private static readonly EnumerationOptions _enumOptions = new EnumerationOptions
-        {
-            MatchType = MatchType.Win32,
-            RecurseSubdirectories = true
-        };
+		private static readonly EnumerationOptions _enumOptions = new EnumerationOptions
+		{
+			MatchType = MatchType.Win32,
+			RecurseSubdirectories = true
+		};
 
-        [Command( "compile" )]
-        private static void Compile( params string[] globs )
-        {
-            var files = globs.SelectMany( glob => Directory.EnumerateFiles( ".", glob, _enumOptions ) );
+		[Command( "compile" )]
+		private static void Compile( params string[] globs )
+		{
+			var files = globs.SelectMany( glob => Directory.EnumerateFiles( ".", glob, _enumOptions ) );
 
-            foreach ( var path in files )
-            {
-                var code = File.ReadAllText( path );
-                var start = Stopwatch.GetTimestamp();
-                _compile.Call( _luaMachine, new ValueSlot[] { code, path } );
-                var end = Stopwatch.GetTimestamp();
-                Console.WriteLine( $"{path} {end - start} {Duration.Format( end - start )}" );
-            }
-        }
+			foreach ( var path in files )
+			{
+				var code = File.ReadAllText( path );
+				var start = Stopwatch.GetTimestamp();
+				_compile.Call( _luaMachine, new ValueSlot[] { code, path } );
+				var end = Stopwatch.GetTimestamp();
+				Console.WriteLine( $"{path} {end - start} {Duration.Format( end - start )}" );
+			}
+		}
 
-        [Command( "gmod" )]
-        private static void LoadGmod()
-        {
-            _luaMachine.Env.Set( "SERVER", true );
-            _luaMachine.Env.Set( "CLIENT", false );
-            _luaMachine.RunFile( "glib_official/garrysmod/lua/includes/init.lua" );
-        }
+		[Command( "gmod" )]
+		private static void LoadGmod()
+		{
+			_luaMachine.Env.Set( "SERVER", true );
+			_luaMachine.Env.Set( "CLIENT", false );
+			_luaMachine.RunFile( "glib_official/garrysmod/lua/includes/init.lua" );
+		}
 
-        [Command( "stats" )]
-        private static void LuaStats() => profilerDump();
+		[Command( "stats" )]
+		private static void LuaStats()
+		{
+			profilerDump();
+		}
 
-        [Command( "run" )]
-        private static void RunFile( string path )
-        {
-            _luaMachine.RunFile( path );
-        }
-    }
+		[Command( "runf" ), Command( "run-file" )]
+		[RawInput]
+		private static void RunFile( string path )
+		{
+			_luaMachine.RunFile( path );
+		}
+
+		[Command( "runs" ), Command( "run-string" )]
+		[RawInput]
+		private static void RunString( string code )
+		{
+			_luaMachine.RunString( code, $"Console.{Interlocked.Increment( ref _consoleCodeCounter ):000}" );
+		}
+	}
 }
