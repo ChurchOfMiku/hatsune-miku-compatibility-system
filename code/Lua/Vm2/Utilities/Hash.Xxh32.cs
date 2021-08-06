419026c3c16f575f3023cea605d1df476e8c6df9
GGG KILLER <gggkiller2@gmail.com>
2021-08-05 21:39:45 -0300

    Implement xxHash 32-bit.

---
+++ code/Lua/Vm2/Utilities/Hash.Xxh32.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Miku.Lua.Vm2.Utilities
+{
+	// This was implemented based on the spec:
+	// https://github.com/Cyan4973/xxHash/blob/dev/doc/xxhash_spec.md
+	internal static partial class Hash
+	{
+		private const uint Xxh32Prime1 = 0x9E3779B1u;  // 0b10011110001101110111100110110001
+		private const uint Xxh32Prime2 = 0x85EBCA77u;  // 0b10000101111010111100101001110111
+		private const uint Xxh32Prime3 = 0xC2B2AE3Du;  // 0b11000010101100101010111000111101
+		private const uint Xxh32Prime4 = 0x27D4EB2Fu;  // 0b00100111110101001110101100101111
+		private const uint Xxh32Prime5 = 0x165667B1u;  // 0b00010110010101100110011110110001
+
+		[MethodImpl( MethodImplOptions.AggressiveInlining )]
+		private static uint ReadUInt32LittleEndian( ReadOnlySpan<byte> data )
+		{
+#if BIG_ENDIAN_SUPPORT
+			return ((uint)data[3] << 24) | ((uint)data[2] << 16) | ((uint)data[1] << 8) | data[0];
+#else
+			return BitConverter.ToUInt32( data );
+#endif //BIG_ENDIAN_SUPPORT
+		}
+
+		[MethodImpl( MethodImplOptions.AggressiveInlining )]
+		private static uint Xxh32Round( uint acc, uint lane )
+		{
+			acc += lane * Xxh32Prime2;
+			acc = BitOperations.RotateLeft( acc, 13 );
+			acc *= Xxh32Prime1;
+			return acc;
+		}
+
+		[MethodImpl( MethodImplOptions.AggressiveInlining )]
+		private static uint Xxh32Avalanche( uint acc )
+		{
+			acc ^= acc >> 15;
+			acc *= Xxh32Prime2;
+			acc ^= acc >> 13;
+			acc *= Xxh32Prime3;
+			acc ^= acc >> 16;
+			return acc;
+		}
+
+		public static uint GetXxh32HashCode( ReadOnlySpan<byte> data, uint seed = 0 )
+		{
+			uint acc;
+			if ( data.Length >= 16 )
+			{
+				uint acc1 = seed + Xxh32Prime1 + Xxh32Prime2;
+				uint acc2 = seed + Xxh32Prime2;
+				uint acc3 = seed + 0;
+				uint acc4 = seed - Xxh32Prime1;
+
+				while ( data.Length >= 16 )
+				{
+					acc1 = Xxh32Round( acc1, ReadUInt32LittleEndian( data ) );
+					data = data[4..];
+					acc2 = Xxh32Round( acc2, ReadUInt32LittleEndian( data ) );
+					data = data[4..];
+					acc3 = Xxh32Round( acc3, ReadUInt32LittleEndian( data ) );
+					data = data[4..];
+					acc4 = Xxh32Round( acc4, ReadUInt32LittleEndian( data ) );
+					data = data[4..];
+				}
+
+				acc = BitOperations.RotateLeft( acc1, 1 )
+					+ BitOperations.RotateLeft( acc2, 7 )
+					+ BitOperations.RotateLeft( acc3, 12 )
+					+ BitOperations.RotateLeft( acc4, 18 );
+			}
+			else
+			{
+				acc = seed + Xxh32Prime5;
+			}
+
+			acc += (uint)data.Length;
+
+			while ( data.Length >= 4 )
+			{
+				var lane = ReadUInt32LittleEndian( data );
+				acc += lane * Xxh32Prime3;
+				acc = BitOperations.RotateLeft( acc, 17 ) * Xxh32Prime4;
+				data = data[4..];
+			}
+
+			for ( var idx = 0; idx < data.Length; idx++ )
+			{
+				var lane = (uint)data[idx];
+				acc += lane * Xxh32Prime5;
+				acc = BitOperations.RotateLeft( acc, 11 ) * Xxh32Prime1;
+			}
+
+			return Xxh32Avalanche( acc );
+		}
+	}
+}
